60bf79a95fd04306ece0b2a7bcd21ec76f899b51
CristianoPirolli <cristianopirolli@hotmail.com>
2026-04-28 22:26:22 -0300

    chore: atualiza configuracoes

--- config/settings.py
+++ config/settings.py
@@ -49,6 +49,9 @@ if DEBUG:
         'http://127.0.0.1:8080',
         'http://localhost:8081',
         'http://127.0.0.1:8081',
+        'http://192.168.2.137:8000',
+        'http://192.168.2.137:8080',
+        'http://192.168.2.137:8081',
     ):
         if origin not in CSRF_TRUSTED_ORIGINS:
             CSRF_TRUSTED_ORIGINS.append(origin)
